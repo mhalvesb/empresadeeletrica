5abc6632341d8cc2457dadb92f93210710b3a69c
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-01-15 19:27:27 -0300

    site atualizado

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Routes>
-        <Route  path="/home" element={<Home/>}></Route>
+        <Route  path="/" element={<Home/>}></Route>
         <Route  path="/about" element={<About/>}></Route>
       </Routes>
       </BrowserRouter>

07024f8b74dcce2b605d74f138cc64e1e38dcf74
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-01-15 19:06:03 -0300

    atualizado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {HashRouter as BrowserRouter, Routes, Route} from "react-router-dom";
 
 import Home from "./containers/home/home.js";
 import About from "./containers/about/about.js";
@@ -10,8 +10,8 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Routes>
-        <Route exact path="/home" element={<Home/>}></Route>
-        <Route exact path="/about" element={<About/>}></Route>
+        <Route  path="/home" element={<Home/>}></Route>
+        <Route  path="/about" element={<About/>}></Route>
       </Routes>
         
       

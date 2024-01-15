219e4162595f87f2260991fdf809b3f729c300ba
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-01-15 19:22:49 -0300

    atualizado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import {HashRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 import Home from "./containers/home/home.js";
 import About from "./containers/about/about.js";
@@ -8,14 +8,12 @@ import About from "./containers/about/about.js";
 function App() {
   return (
     <div className="App">
-      <HashRouter>
+      <BrowserRouter>
       <Routes>
         <Route  path="/home" element={<Home/>}></Route>
         <Route  path="/about" element={<About/>}></Route>
       </Routes>
-        
-      
-      </HashRouter>
+      </BrowserRouter>
       
       
     </div>
